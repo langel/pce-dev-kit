17475b63feafc8526d82deac8856313809eb2ad0
langel <knoxburry@gmail.com>
2021-12-09 22:29:43 -0500

    actually understanding some things

--- README.md
+++ README.md
@@ -2,10 +2,21 @@
 This is a collection of resources found around the web pertaining to homebrew development for the PC Engine aka TurboGrafx-16.
 
 ## CPU and Other Processors
-The PCE has a custom 65C02 processor sometimes referred to by its CMOS designation 65CS02, but more commonly known as the HuC6280. The 6280 package also holds the programmable sound generator (sometimes referred to as an SCC chip by VGM nerds).
+The PCE has a custom 65C02 processor sometimes referred to by its CMOS designation 65CS02, but more commonly known as the HuC6280. The 6280 has extra opcodes specific for quicker operations with the console's Video Display Controller. The 6280 package also holds the programmable sound generator (sometimes referred to as an SCC chip by VGM nerds).
 
-## HuC and MagicKit
-DOS literally requires DOSBOX on a modern Windows PC.
+## MagicKit's HuC and PCEAS
+HuC is a library and transpiler for making PC Engine ROMs using the C language. It converts the project into assembly and the compiles with PCEAS (PC Engine ASembler). DOS version literally requires DOSBOX on a modern Windows PC. Windows version works on modern installs with Command Prompt. OSX version could easily predate OSX since the HuC/PCEAS source has not been updated since Dec 2000.
 
 ## Audio Notes
-MML, Squirrel, Deflemask
+MML, Squirrel, Deflemask -- maybe some more notes someday haha  xD
+
+## Links / Resources
+- [ChibiAkumas General Knowledge](https://www.chibiakumas.com/6502/pcengine.php)
+- [ChibiAkumas Intro to 6280 OpCodes](https://www.chibiakumas.com/6502/pcengine.php)
+- [MagicKit Documentaion](http://www.magicengine.com/mkit/doc.html)
+- [Archaic Pixels Dev Wiki](http://archaicpixels.com/Main_Page)
+- [ObeyBrew Tutorials](http://obeybrew.com/tutorials.html)
+- [Chris Covell's CPU / Emulator Tester](https://chrismcovell.com/CPUTest/index.html)
+- [Alddee Homebrew Game w/ source](http://www.sebastianmihai.com/turbografx-alddee.html)
+- [Squirrel Music Maker for PCE](http://www.aetherbyte.com/aetherbyte-squirrel_for_pc-engine_and_turbografx-16.html)
+- [An Immolated Hot Dog](https://tikolu.net/emojimix/%F0%9F%8C%AD+%F0%9F%94%A5)
--- test.asm
+++ test.asm
@@ -1,9 +1,14 @@
     ;#listing2.asm
     ;------------
 
+timerport    .equ $0C00
+timer_cnt    .equ timerport
+timer_ctrl   .equ timerport+1        
+
 	.zp
 	.org $010
 zp_test: .ds 1
+fine_tune: .ds 1
 	.bss
  	.org $2680
 vsync_cnt:	.ds 1	; counter for 'wait_vsync' routine 
@@ -15,6 +20,10 @@ vsync_cnt:	.ds 1	; counter for 'wait_vsync' routine
 start_up:
 	; disable interrupts
 	sei      
+	; select the 7.16 MHz clock
+	csh
+	; clear decimal flag
+	cld
 	; initialize SP to $ff
 	lda #$ff
 	txs
@@ -26,16 +35,93 @@ start_up:
 	; $2000 - $3fff
 	lda #$f8     
 	tam #$01
+
+	; clear all of RAM
 	; set 1st byte of ram to desired default value
 	lda #$00
 	sta $2000
 	; propogate 1st byte value to all of ram
 	tii $2000, $2001, $1fff
 
+	; reset timer?
+	stz timer_ctrl
+
+	; init VDC
+	lda #$05  ; CR
+	sta $0000
+	lda #$00
+	sta $0002
+	sta $0003
+	lda #$06  ; RCR
+	sta $0000
+	lda #$00
+	sta $0002
+	sta $0003
+	lda #$07  ; BXR
+	sta $0000
+	lda #$00
+	sta $0002
+	sta $0003
+	lda #$08  ; BYR
+	sta $0000
+	lda #$00
+	sta $0002
+	sta $0003
+	lda #$09  ; MWR
+	sta $0000
+	lda #$00  ; bits 4-6 set bg virtual size
+	sta $0002
+	lda #$00
+	sta $0003
+	lda #$0a  ; HSR
+	sta $0000
+	lda #$02
+	sta $0002
+	lda #$02
+	sta $0003
+	lda #$0b  ; HDR
+	sta $0000
+	lda #$1f
+	sta $0002
+	lda #$04
+	sta $0003
+	lda #$0c  ; VPR (video sync register)
+	sta $0000
+	lda #$02
+	sta $0002
+	lda #$10
+	sta $0003
+	lda #$0d  ; VDW
+	sta $0000
+	lda #$e7
+	sta $0002
+	lda #$00
+	sta $0003
+	lda #$0e  ; VCR
+	sta $0000
+	lda #$0c
+	sta $0002
+	lda #$00
+	sta $0003
+	lda #$0f  ; DCR
+	sta $0000
+	lda #$10
+	sta $0002
+	lda #$00
+	sta $0003
+	lda #$13  ; SATB
+	sta $0000
+	lda #$00
+	sta $0002
+	lda #$7f
+	sta $0003
+
+
+
 	; enable background and sprites
 	lda #$05   ; select the control register
 	sta $0000
-	lda #$c8   ; enable bits 3, 6 and 7
+	lda #$cc   ; enable bits 3, 4, 6 and 7
 	sta $0002
 	lda #$00
 	sta $0003
@@ -44,13 +130,145 @@ start_up:
 	lda #$06   ; select the RCR register
 	sta $0000
 	; first scanline is #$40 or #64
-	lda #$40
+	lda #$80
 	sta $0002  ; write lo byte
-	lda #$01
+	lda #$00
 	sta $0003  ; write hi byte
+
+
+	; fill palette data
+	ldy #$00
+	lda #$00
+	sta $0402
+	sta $0403
+.palfill:
+	tya
+	sta $0404
+	lda #$00
+	sta $0405
+	iny
+	bne .palfill
+.palfill2:
+	tya
+	sta $0404
+	lda #$01
+	sta $0405
+	iny
+	bne .palfill2
+
+
+	; fill VRAM Bg Attribute Table
+	lda #$00
+	sta $0000
+	sta $0002
+	sta $0003
+	lda #$02
+	sta $0000
+	ldy #$00
+.batfill:
+	lda #$10
+	tya
+	lsr A
+	lsr A
+	lsr A
+	sta $0002
+	;lda #$00
+	tya
+	asl A
+	asl A
+	asl A
+	asl A
+	sta $0003
+	iny
+	bne .batfill
+.batfill2:
+	lda #$10
+	tya
+	lsr A
+	lsr A
+	sta $0002
+	;lda #$00
+	tya
+	asl A
+	asl A
+	asl A
+	asl A
+	sta $0003
+	iny
+	bne .batfill2
+	
+	; write to tile $10
+	lda #$00
+	sta $0000
+	sta $0002
+	lda #$10
+	sta $0003
+	lda #$02
+	sta $0000
+	ldy #$00
+.tilefill:
+	tya
+	sta $0002
+	lda #$00
+	sta $0003
+	iny
+	bne .tilefill
+	ldy #$00
+.tilefill2:
+	tya
+	sta $0002
+	lda #$00
+	sta $0003
+	iny
+	bne .tilefill2
+.tilefill3:
+	tya
+	sta $0002
+	lda #$00
+	sta $0003
+	iny
+	bne .tilefill3
+.tilefill4:
+	tya
+	sta $0002
+	lda #$00
+	sta $0003
+	iny
+	bne .tilefill4
 
 	; enable interrupts
 	cli
+
+
+
+; INIT PSG
+init_psg:
+	lda #$ff  ; global volume full
+	sta $0801
+	lda #$00  ; select channel 0
+	sta $0800
+	lda #$4e  ; fine tune
+	sta $0802
+	sta fine_tune
+	lda #$05  ; coarse tune
+	sta $0803
+	lda #$40  ; reset wavetable position
+	sta $0804
+	lda #$00  ; turn off to enable wave write
+	sta $0804
+	ldy #$00  ; write wave table loop
+.loading_square:
+	lda square,y
+	sta $0806
+	iny
+	cpy #$20
+	bne .loading_square
+	lda #$ff  ; set L/R volume full
+	sta $0805 
+	lda #$9f  ; enable channel and full volume
+	sta $0804
+
+
 
 	ldy #$00
 
@@ -60,37 +278,60 @@ loop:
 
 
 irq1:
-	sei
+	pha
+	phx
+	phy
+	; READ THE STATUS REGISTER TO
+	; RESET THE VDC IRQ FLAG
 	lda $0000
-	inc zp_test + 10
+	; increment fine tune
+	inc fine_tune
+	lda fine_tune
+	sta $0802
+	; check loop counter position
 	inc $2020
+	lda $2020
+	cmp #$08
+	bne .cleanup
+	; rotate bg color
 	lda #$00
 	sta $0402
 	sta $0403
-	iny
-	tya 
-	;and #$03
+	inc $2021
+	lda $2021
 	sta $0404
-	sta $0405
+	inc zp_test + 10
+	tya
 	sta zp_test
 	sta $2000
 	sta $2004
-	inc $2022
-	sta $2200
-	sta $2222
-	sta $2333
-	sta $3000
-	sta $3ffe
-	sta $3fff
-	cli
+	; reset loop
+	lda #$00
+	sta $2020
+.cleanup:
+	ply
+	plx
+	pla
 	rti
+
+interrupt_rti:
+	rti
+
+square:
+	.db $1b,$1d,$1e,$1e,$1f,$1f,$1f,$1f,$1f,$1f,$1f,$1f,$1e,$1e,$1d,$1b,$04,$02,$01
+	.db $01,$00,$00,$00,$00,$00,$00,$00,$00,$01,$01,$02,$04
 
 	.include "windows huc\include\pce\font.inc"
 
 	; we are still inside bank $00
 	; IRQ1 VDC interrupt vector
-	.org $fff8
+;	.org $fff8
+;	.dw irq1
+	.org $fff6
+	.dw interrupt_rti
 	.dw irq1
+	.dw interrupt_rti
+	.dw interrupt_rti
 	; program reset vector
 	.org $fffe     
 	.dw start_up
