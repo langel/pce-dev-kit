f78da4e17d2fc8305b48a334521d3bc34733dbd0
langel <knoxburry@gmail.com>
2021-12-14 17:24:20 -0500

    audio conversion and playback milestone

--- audio tools/wav2pce/wav2pce.c
+++ audio tools/wav2pce/wav2pce.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,7 +45,7 @@ int main(int argc, char* args[]) {
 	printf("channel count : %d\n", channel_count);
 	uint32_t sample_rate;
 	fread(&sample_rate, 4, 1, fp);
-	printf("sample rate : %d\n", sample_rate);
+	printf("sample rate : %dHz\n", sample_rate);
 	fseek(fp, 34, SEEK_SET);
 	uint16_t bit_depth;
 	fread(&bit_depth, 2, 1, fp);
@@ -60,41 +61,53 @@ int main(int argc, char* args[]) {
 	printf("wav file playback length : %fs\n", (float) sample_count_per_channel / (float) sample_rate);
 
 	// PC Engine clock and audio
-	float pce_cpu_clock = 7160.f; // 7.16 mHz
+	float pce_cpu_clock = 7160000.f; // 7.16 mHz
+	// XXX timer rate could vary
 	float pce_timer_rate = pce_cpu_clock / 1024.f;
-	float pce_sample_rate = pce_cpu_clock / pce_timer_rate;
 
 	// conversion adjusters
-	float sample_ratio = pce_sample_rate / sample_rate;
+	float sample_ratio = pce_timer_rate / sample_rate;
 	float bit_ratio = 0.f;
 	float bit_offset = 0.f;
-
-	// set adjusters and error handle weirdos
-	fseek(fp, 44, SEEK_SET);
-	if (bit_depth == 8 || bit_depth == 16) {
-		bit_ratio = 5.f / (float) bit_depth;
-	}
-	else if (bit_depth == 32) {
-		bit_ratio = 5.f / 2.f;
+	// XXX target bit depth could vary
+	uint8_t pce_bit_depth = 5;
+	if (bit_depth == 32) {
+		bit_ratio = (float) pce_bit_depth / 2.f;
 		bit_offset = 1.f;
 	}
 	else {
-		printf("%d bit depth not supported\n", bit_depth);
-		return 1;
+		bit_ratio = (float) (2 << pce_bit_depth) / (float) (2 << bit_depth);
 	}
 	
-	// create target file
+	// create target file and spit out info
 	char target_filename[2048];
 	char * extension = strrchr(args[1], '.');
 	strncpy(target_filename, args[1], extension - args[1]);
 	strcat(target_filename, ".bin");
 	printf("\ntarget file : %s\n", target_filename);
+	FILE * tp = fopen(target_filename, "w");
+	printf("target playback rate : %fHz\n", pce_timer_rate);
+	printf("target bit depth : %d\n", pce_bit_depth);
+	printf("bit ratio : %f\n", bit_ratio);
+	printf("sample ratio : %f\n", sample_ratio);
 
 	// process / convert / save
-	printf("target playback rate : %fkHz\n", pce_timer_rate);
+	fseek(fp, 44, SEEK_SET);
+	// XXX need separate loop for 32bit float wav data
+	uint16_t source_data;
+	uint8_t target_data;
+	for (int i = 0; i < data_length; i += byte_size) {
+		fread(&source_data, byte_size, 1, fp);
+		source_data += (65536 >> 1);
+		target_data = (uint8_t) ((float) source_data * bit_ratio);
+		fwrite(&target_data, 1, 1, tp);
+//		printf("source : %d\t target : %d\n", source_data, target_data);
+	}
+
 
 	// cleanup and shutdown
 	fclose(fp);
+	fclose(tp);
 
 	return 0;
 }
---
+++ pizzaball.bat
@@ -0,0 +1,2 @@
+"windows huc\bin\pceas.exe" -S pizzaball.asm
+copy pizzaball.pce "D:\games\BizHawk-2.7-win-x64\PC Engine\"
